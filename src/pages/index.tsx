5e2e351d6fae7a350e4c4179f82a2fea6264b580
Sasi <me@sasi.codes>
2022-03-08 22:25:13 +0530

    fix: mobile view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import PartnerCard from 'src/components/PartnerCard'
 export default function Home() {
   return (
     <Layout>
-      <div className="container max-w-4xl py-6 mx-auto">
+      <div className="container max-w-4xl px-3 py-6 mx-auto md:px-0">
         <div className="text-center">
           <a className="text-lg tracking-wider text-gray-400" href="">
             {`=> To help understand how "liquid loans" work, check out our docs page <=`}
