00aa1981fc45ece7e1ccde4d9269e6735b2432f7
Sasi <me@sasi.codes>
2022-05-22 11:55:59 +0530

    chore: font size

--- src/components/liquidity-mining/index.tsx
+++ src/components/liquidity-mining/index.tsx
@@ -7,11 +7,11 @@ const LiquidityMining = () => {
   return (
     <div className="container max-w-2xl px-3 py-6 mx-auto space-y-6 md:px-0">
       <div>
-        <div className="mb-1 text-xl text-gray-400">Loans</div>
+        <div className="mb-1 text-2xl text-gray-400">Loans</div>
         <Loan />
       </div>
       <div>
-        <div className="mb-1 text-xl text-gray-400">Liquidity</div>
+        <div className="mb-1 text-2xl text-gray-400">Liquidity</div>
         <Liquidity />
       </div>
     </div>
